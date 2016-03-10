8602230e3954d2e89059366462d3349d70343759
Kevo <kevo548@gmail.com>
2016-03-10 16:53:40 -0500

    pre class push of work so far

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Giphy API
+// Kevin Haas 2016
+
+// tooltip for hover that tells you about start/stop with click? or will this be annoying
+
+
+var defaultCategories = ["Music", "Computers", "Food", "Fractals", "Phish", "Guitar", "Dreams", "Boston Terrier", "Dogs and Cats", "Jerry Garcia", "Venture Bros"]
+
+// intro
+$("#topRightDiv, #bottomRightDiv, #searchArea, #buttons").hide();
+$("#introImg").fadeOut(4000);
+$("#topRightDiv, #bottomRightDiv, #searchArea, #buttons").delay(3300).fadeIn(3000);
