407912b4f6890217fb2946d383f6af7b2ec517a8
Kevo <kevo548@gmail.com>
2016-03-15 16:24:22 -0400

    gifs into grid with rating. used bootstrap with jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,23 @@
 // Giphy API
 // Kevin Haas 2016
 
-//use data attr and .val().trim()
-
-// tooltip for hover that tells you about start/stop with click? or will this be annoying
-
-// intro - make is so everything is dynamically generated and doesn't show up quickly
-// when the page loads
-
-// animate buttons on click with css3 dynamically
-
-// make buttons fade in on add. can probably use jQ to set style display:none and then fade in?
-
 // make api corner image a sticky footer in that spot
 
-// make buttons into a constructor and push new search to it?
-
 var apiKey = "dc6zaTOxFJmzC";
+
+var maxResults = "25";
 
 var buttons = ["music", "computers", "food", "fractals", "phish", "guitar", "dreams", "boston terrier", "dogs and cats", "coffee", "venture bros"];
 
 // into fade
-$("#topRightDiv, #bottomRightDiv, #searchArea, #buttonDiv, #results").hide();
+$("#topRightDiv, #bottomRightDiv, #searchArea, #buttonDiv, #results, #instructions").hide();
 $("#introImg").fadeOut(4000);
-$("#topRightDiv, #bottomRightDiv, #searchArea, #buttonDiv, #results").delay(3300).fadeIn(3000);
+$("#topRightDiv, #bottomRightDiv, #searchArea, #buttonDiv, #results, #instructions").delay(3300).fadeIn(3000);
 
 function loadButtons() {
     
     for (var i = 0; i < buttons.length; i++) {
-        $("#buttonDiv").append("<div data-search='" + buttons[i] + "' class='button badge'>" + buttons[i]);
-        
+        $("#buttonDiv").append("<div data-search='" + buttons[i] + "' class='button badge'>" + buttons[i]);  
     }
 }
 
@@ -42,7 +30,6 @@ document.onkeyup = function (playerInput) {
         buttons.push(this);
         $("#buttonDiv").append("<div data-search='" + newBtn + "' class='button badge'>" + newBtn);
         $("#searchField").val("");
-        
     }
 }
 
@@ -63,13 +50,11 @@ $("#clearBtn").on("click", function clearBtn() {
 $("body").off('click').on("click", ".button", function displayResults() { 
     
     
-    // allows page to scroll. it's set to hidden at first because the intro gif would show a vertical scroll bar with overflow visible
+// allows page to scroll. it's set to hidden at first because the intro gif would show a vertical scroll bar with overflow visible
     $("body").css("overflow", "visible");
     
-
-    
     var search = $(this).attr("data-search");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey;
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=" + maxResults;
 
     $.ajax({
         url: queryURL, 
@@ -80,40 +65,46 @@ $("body").off('click').on("click", ".button", function displayResults() {
    
     
         var results = response.data;
-           
+        
+        
+// creation of the results including the rating
         for (var i = 0; i < results.length; i++) {
             console.log(results[i].images.fixed_height.url);
             
-            var result = $("<img data-still='" + results[i].images.fixed_height_still.url + "' data-anim='" + results[i].images.fixed_height.url + "' class='result'>");
+            var resultRating = results[i].rating;
             
+            var rating = $("<p>").text("Rating: " + resultRating);
+            
+            var result = $("<img data-still='" + results[i].images.fixed_height_still.url + "' data-anim='" + results[i].images.fixed_height.url + "' class='center-block result'>" + "<p id='rating'>");
+
             var gifStill = $(result).attr("data-still");
             
             var gifAnim =  $(result).attr("data-anim");
             
+            var resultDiv = $("<div class='col-md-4'>")
+            
             result.attr("src", gifStill);
             
-            $("#resultDiv").prepend(result);            
-           
+            resultDiv.append(result);
             
-$("body").off('click').on("click", ".result", function startStopGif() {
+            
+            $("#resultDiv").prepend(resultDiv);
+            $("#rating").append(rating);
+   
+// animates the image and stops it on click          
+$(document).on("click", ".result", function startStopGif() {
 
     if ($(this).attr("src") == $(this).data('still') ) {
-        $(this).attr('src', $(this).data('anim'));
+        $(this).attr("src", $(this).data("anim"));
     }
     else{
-        $(this).attr('src', $(this).data('still'));
+        $(this).attr("src", $(this).data("still"));
     }
-        
-    
 });
-            }
+           
+    }
             
     });      
 });
          
-        
-
-
 loadButtons();
-
-// make the animated/still into a variable that changes onclick
